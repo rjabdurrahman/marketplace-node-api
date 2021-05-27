b18e1f55f123487f3d99d6a28628b413b5fbbb5b
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-05-27 16:20:36 +0600

    Done: First

--- controllers/readXLSheader.js
+++ controllers/readXLSheader.js
@@ -1,19 +1,28 @@
 const ExcelJS = require('exceljs');
+const https = require('https');
+const fs = require('fs');
 
 async function readXLSheader(req, res) {
+    const file = fs.createWriteStream('sample1_v1.xlsx');
     let fileURL = req.body.ReadXLSHeader.file[0].$.url;
     let rowNo = req.body.ReadXLSHeader.row[0].$.no;
     const wb = new ExcelJS.Workbook();
-    await wb.xlsx.readFile('sample1_v1.xlsx');
-    let ws = wb.getWorksheet(1);
-    let cells = ws.getRow(4).model.cells;
-    let valueCells = cells.filter(c => c.value);
-    let structedData = {};
-    valueCells.forEach(c => {
-        let key = c.address.replace(/\d/, '');
-        structedData['Column' + key] = c.value;
+    https.get('https://' + fileURL, response => {
+        let stream = response.pipe(file);
+        stream.on("finish", async () => {
+            await wb.xlsx.readFile('sample1_v1.xlsx');
+            // await wb.xlsx.read(response.buffer());
+            let ws = wb.getWorksheet(1);
+            let cells = ws.getRow(rowNo).model.cells;
+            let valueCells = cells.filter(c => c.value);
+            let structedData = {};
+            valueCells.forEach(c => {
+                let key = c.address.replace(/\d/, '');
+                structedData['Column' + key] = c.value;
+            });
+            res.send(structedData);
+        });
     });
-    res.send(structedData);
 }
 
 module.exports = { readXLSheader };
